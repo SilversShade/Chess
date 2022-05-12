9247bdf5a869cb7819a494c180eb9b52d77c662b
SilversShade <shantemgardenix@gmail.com>
2022-05-12 13:05:53 +0500

    en passant implemented

--- Pieces/Pawn.cpp
+++ Pieces/Pawn.cpp
@@ -1,9 +1,24 @@
 #include "Pawn.h"
-#include <iostream>
 
 Pawn::Pawn(PieceColor color, int x, int y): Piece(color, x, y) {
     this->atStartingPos = true;
     this->type = PieceType::PAWN;
+}
+
+bool Pawn::enPassantConditionAt(int x, int y, std::vector<Piece*> &pieces) {
+    for (const auto &piece:pieces) {
+        if (piece->getPosX() == x && piece->getPosY() == y && piece->getColor() != this->getColor()) {
+            if (Pawn* p = dynamic_cast<Pawn*>(piece)) {
+                if (p->enPassantCondition) {
+                    piece->getSprite()->setPosition(-100,-100);
+                    piece->setPosX(-1);
+                    piece->setPosY(-1);
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
 }
 
 bool Pawn::isMoveValid(int xEnd, int yEnd, std::vector<Piece*> &pieces) {
@@ -13,6 +28,8 @@ bool Pawn::isMoveValid(int xEnd, int yEnd, std::vector<Piece*> &pieces) {
             return false;
         if ((this->getColor() == PieceColor::WHITE ? this->getPosY()-yEnd==2 : yEnd-this->getPosY()==2) && !isCellFreeAt(xEnd,this->getColor()==PieceColor::WHITE ? yEnd+1 : yEnd-1,pieces))
             return false;
+        if (this->getColor() == PieceColor::WHITE ? this->getPosY()-yEnd==2 : yEnd-this->getPosY()==2)
+            this->enPassantCondition = true;
         this->atStartingPos = false;
         return true;
     }
@@ -20,15 +37,20 @@ bool Pawn::isMoveValid(int xEnd, int yEnd, std::vector<Piece*> &pieces) {
     if (this->getPosX() == xEnd && (this->getColor() == PieceColor::WHITE ? this->getPosY()-yEnd==1 : yEnd-this->getPosY()==1)) {
         if (!isCellFreeAt(xEnd, yEnd, pieces))
             return false;
+        this->enPassantCondition = false;
         return true;
     }
     //en passant
-
+    if ((this->getColor() == PieceColor::WHITE ? this->getPosY()-yEnd==1 : yEnd-this->getPosY()==1) && (this->getPosX()-xEnd==1 || xEnd-this->getPosX()==1)
+        && enPassantConditionAt(xEnd, this->getColor() == PieceColor::WHITE ? yEnd+1 : yEnd-1, pieces))
+            return true;
     // diagonal capture
     PieceColor colorAtDest = checkColorAt(xEnd, yEnd, pieces);
     if (colorAtDest == PieceColor::EMPTY)
         return false;
-    if ((this->getColor() == PieceColor::WHITE ? this->getPosY()-yEnd==1 : this->getPosY()-yEnd==-1) && (this->getPosX()-xEnd==1 || xEnd-this->getPosX()==1) && colorAtDest!=this->getColor())
+    if ((this->getColor() == PieceColor::WHITE ? this->getPosY()-yEnd==1 : this->getPosY()-yEnd==-1) && (this->getPosX()-xEnd==1 || xEnd-this->getPosX()==1) && colorAtDest!=this->getColor()) {
+        this->enPassantCondition = false;
         return true;
+    }
     return false;
 }
--- Pieces/Pawn.h
+++ Pieces/Pawn.h
@@ -4,6 +4,8 @@
 class Pawn : public Piece {
 private:
     bool atStartingPos;
+    bool enPassantCondition = false;
+    bool enPassantConditionAt(int, int, std::vector<Piece*> &pieces);
 public:
     Pawn(PieceColor, int, int);
     bool isMoveValid(int xEnd, int yEnd, std::vector<Piece*> &pieces) override;
