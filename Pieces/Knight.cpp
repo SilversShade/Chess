9e801d6e1c01b29366df3fd44ec693db0e556b93
Alex <96792931+SilversShade@users.noreply.github.com>
2022-05-20 09:20:02 +0500

    Update Knight.cpp

--- Pieces/Knight.cpp
+++ Pieces/Knight.cpp
@@ -5,6 +5,8 @@ Knight::Knight(PieceColor color, int x, int y) : Piece(color, x, y){
 }
 
 bool Knight::isMoveValid(int xEnd, int yEnd, std::vector<Piece*> &pieces) {
+    if (this->getPosX() == -1)
+        return false;
     PieceColor colorAtDest = checkColorAt(xEnd, yEnd, pieces);
     if (((std::abs(this->getPosX()-xEnd)==1 && std::abs(this->getPosY()-yEnd)==2) || (std::abs(this->getPosX()-xEnd)==2 && std::abs(this->getPosY()-yEnd)==1)) && colorAtDest != this->getColor())
         return true;
